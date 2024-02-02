00215b8dbe570e80149c1018cc80b5fcea70e09f
JaiChandwani <92116987+JaiChandwani@users.noreply.github.com>
2024-02-02 16:29:35 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+        //    For room selection
+        //    For selection singlebedroom
+       singlebedBook = () => {
+            document.getElementById("room").value = "economic"
+        }
+
+        //    For selection singlebedroom
+        doublebedBook = () => {
+            document.getElementById("room").value = "Deluxe"
+        }
+
+         //    For selection singlebedroom
+        triplebedBook = () => {
+            document.getElementById("room").value = "Exclusive"
+        }
+
+        //    For selection singlebedroom
+        quadbedBook = () => {
+            document.getElementById("room").value = "superDeluxe"
+        }
+
+
+
+        // After Booking the room confirmation.
+        booked = () => {
+            var x = document.getElementById("name").value;
+            var y = document.getElementById("tel").value;
+            var z = document.getElementById("room").value;
+            var j = document.getElementById("date").value;
+
+            document.getElementById("form").innerHTML = (`Thank you for choosing us. ${x} <br>Your room has been booked with id: ODR/223023/12314<br>Room Type: ${z}<br>On: ${j}<br>kindly keep this refence id with you It'll be asked at time of check in.<br><br><i>Payment will be done at the time of check in.</i>`);
+            document.getElementById("form").style.textAlign = "center"
+        }
